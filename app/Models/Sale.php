d85f84a8d5ad0f7f6c790db71ae558ce99ac58c6
troshev vladimir <troshevvladimir@gmail.com>
2023-04-24 14:09:30 +0300

    begit sales

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'type_id',
+        'client_id',
+        'paid',
+        '1c_id',
+        'sale_items'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'placement_date',
+        'payed_date',
+        'start', // рекламная компания
+        'end'
+    ];
 }
--- database/migrations/2023_04_24_103003_create_sales_table.php
+++ database/migrations/2023_04_24_103003_create_sales_table.php
@@ -13,16 +13,21 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->date('payed_date');
             $table->date('placement_date');
+            $table->date('payed_date');
+            $table->boolean('paid');
             $table->date('start');
             $table->date('end');
             $table->string();
             $table->integer('1c_id');
             $table->json('sale_items');
-            $table->id();
-            $table->id();
-            
+            // $table->foreignId('legal_id')->constrained('legals');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('client_id')->constrained('clients');
+            $table->foreignId('agent_id')->constrained('agents');
+            $table->foreignId('type_id')->constrained('sales_types');
+            $table->foreignId('smi_id')->constrained('smis');
+
             $table->timestamps();
         });
     }
