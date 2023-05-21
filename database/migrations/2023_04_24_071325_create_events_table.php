52b5df752377bfe44cd526764c740352bde29076
Troshev Vladimir <v.troshev@3h.ru>
2023-05-21 16:04:08 +0300

    fix

--- database/migrations/2023_04_24_071325_create_events_table.php
+++ database/migrations/2023_04_24_071325_create_events_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->boolean('result')->default(false);
             $table->date('fulfilled_date')->nullable();
             $table->date('appointment_date');
-            $table->text('comment')->default(false);
+            $table->text('comment');
 
             $table->unsignedBigInteger('type_id');
             $table->foreign('type_id')->references('id')->on('event_types');
